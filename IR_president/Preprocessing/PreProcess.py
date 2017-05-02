b4bee167bbadb6a2dfdfd3c202887fcbc76ca31a
xuez <xuezhihao@gmail.com>
2017-05-02 18:15:22 -0400

    extract anchor tag info

--- IR_president/Preprocessing/PreProcess.py
+++ IR_president/Preprocessing/PreProcess.py
@@ -12,8 +12,7 @@ f_dir = "../../Presidents"
 
 
 def find_file_list():
-    file_list = os.listdir(f_dir)
-    return file_list
+    return sorted(f for f in os.listdir(f_dir))
 
 def parse_one_file(f_name):
     f_path = f_dir + '/' + f_name
@@ -22,10 +21,16 @@ def parse_one_file(f_name):
     file.close()
     title = soup.title.string
     a_tag = soup.find_all('a')
-    print "a tag", a_tag
-    print "a tag string", a_tag[1].string
-    print "a tag len", len(a_tag)
-    print "soup content", soup.contents
+    link_list = []
+    for a in a_tag:
+        if a.get('href') is not None:
+            href = a.get('href')
+            title = a.get('title')
+            link_list.append((href, title))
+    print link_list[0]
+    print link_list[0][0]
+    print link_list[0][1]
+    print len(a_tag)
 
 def main():
     f_list = find_file_list()
